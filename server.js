0119b7899cd42187c9a04c91096d7a0fdf1ab0d0
Sean Siganoff <nationwideclassics@yahoo.com>
2022-08-29 21:38:21 -0400

    asdlfjk

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ app.use(express.json());
 
 
 app.get('/', (req, res) => {
-    res.json('Hello')
+    res.json('Hello Sean')
 })
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)

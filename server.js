e5871f57a493355ea7cac1665c73de6aa1bb8bb5
Sean Siganoff <nationwideclassics@yahoo.com>
2022-08-29 21:33:07 -0400

    testing

--- server.js
+++ server.js
@@ -2,8 +2,12 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
+app.use(express.json());
 
 
+app.get('/', (req, res) => {
+    res.json('Hello')
+})
 app.listen(PORT, () => {
     console.log(`Listening on port ${PORT}`)
 })
